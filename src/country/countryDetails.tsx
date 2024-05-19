4875d7393ed2d26d517400d186ddffe18c41866d
FlowerWineMonk <hidekitakuya0@gmail.com>
2024-05-20 00:03:24 +0500

    Added DetailedCountryHandler

--- src/country/countryDetails.tsx
+++ src/country/countryDetails.tsx
@@ -1,36 +1,75 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { SetStateAction, useEffect } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 
 export interface Country {
   name: {
     common: string;
   };
-  flags: any;
+  flags: {
+    png: string;
+  };
   region: string;
   population: number;
   capital: string;
   subregion: string;
-  currencies: [];
-  languages: [];
-  borders: [];
+  currencies: { [key: string]: { name: string } };
+  languages: { [key: string]: string };
+  borders: string[];
 }
 
-export const CountryInfo: React.FC<Country> = (country) => {
+export const CountryInfo: React.FC<Country & { onClick: () => void }> = ({
+  name,
+  flags,
+  region,
+  population,
+  capital,
+  onClick,
+}) => {
   return (
-    <div className="country-info">
-      <img src={country.flags.png} />
-      <h2>{country.name.common}</h2>
-      <p>Region: {country.region}</p>
-      <p>Capital: {country.capital}</p>
-      <p>Population: {country.population.toLocaleString()}</p>
-      <p>Sub Region: {country.subregion}</p>
-      <p>Currencies: </p>
+    <div className="country-info" onClick={onClick}>
+      <img src={flags.png} />
+      <h2>{name.common}</h2>
+      <p>Region: {region}</p>
+      <p>Capital: {capital}</p>
+      <p>Population: {population.toLocaleString()}</p>
     </div>
   );
 };
 
+const DetailedCountryInfo: React.FC<Country> = (country) => {
+  const {
+    name,
+    flags,
+    region,
+    population,
+    capital,
+    subregion,
+    currencies,
+    languages,
+    borders,
+  } = country;
+
+  const currencyNames = Object.values(currencies)
+    .map((currency) => currency.name)
+    .join(", ");
+  const languageNames = Object.values(languages).join(", ");
+
+  return (
+    <div className="detailed-country-info">
+      <img src={flags.png} alt={`Flag of ${name.common}`} />
+      <h2>{name.common}</h2>
+      <p>Region: {region}</p>
+      <p>Capital: {capital}</p>
+      <p>Population: {population.toLocaleString()}</p>
+      <p>Sub Region: {subregion}</p>
+      <p>Currencies: {currencyNames}</p>
+      <p>Languages: {languageNames}</p>
+      <p>Borders: {borders.join(", ")}</p>
+    </div>
+  );
+};
 const Countries = ({
   data,
   setData,
@@ -38,6 +77,7 @@ const Countries = ({
   data: Country[];
   setData: React.Dispatch<SetStateAction<any>>;
 }) => {
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
   useEffect(() => {
     (async () => {
       try {
@@ -56,14 +96,26 @@ const Countries = ({
     })();
   }, [setData]);
 
+  const handleCountryClick = (country: Country) => {
+    setSelectedCountry(country);
+  };
+
   return (
-    <>
-      <div>
-        {data.map((country: any) => (
-          <CountryInfo key={country.name.common} {...country} />
-        ))}
-      </div>
-    </>
+    <div>
+      {selectedCountry ? (
+        <DetailedCountryInfo {...selectedCountry} />
+      ) : (
+        <div>
+          {data.map((country) => (
+            <CountryInfo
+              key={country.name.common}
+              {...country}
+              onClick={() => handleCountryClick(country)}
+            />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 export default Countries;
